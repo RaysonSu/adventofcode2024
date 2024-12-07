b47a4a875dc4ff838a112eac7ef05b7767573e97
Rayson Su <rsu@connect.kellettschool.com>
2024-12-07 09:08:07 +0800

    Day 6
    oh no run time

--- Day 6/main.py
+++ Day 6/main.py
@@ -4,17 +4,77 @@ from functools import *
 from itertools import *
 from math import *
 
+def find_locations(grid: list[str]) -> tuple[bool, set[tuple[int, int]]]:
+    locations_seen: set[tuple[int, int]] = set()
+    states_seen: set[tuple[int, int, int, int]] = set()
+    
+    if "^" not in "".join(grid):
+        return False, locations_seen
+
+    width = len(grid[0])
+    height = len(grid)
+    index = "".join(grid).index("^")
+    x = index % width
+    y = index // width
+    dx = 0
+    dy = -1
+
+    while True:
+        locations_seen.add((x, y))
+        states_seen.add((x, y, dx, dy))
+        x += dx
+        y += dy
+
+        if x < 0 or x >= width:
+            break
+
+        if y < 0 or y >= height:
+            break
+
+        if grid[y][x] == "#":
+            x -= dx
+            y -= dy
+            dx, dy = -dy, dx
+        
+        if (x, y, dx, dy) in states_seen:
+            return False, locations_seen
+    
+    return True, locations_seen
+
 def main_part_1(inp: list[str]) -> int:
-    return 0
+    return len(find_locations(inp)[1])
 
 def main_part_2(inp: list[str]) -> int:
-    return 0
+    total = 0
+
+    for x, y in find_locations(inp)[1]:
+        if inp[y][x] == "^":
+            continue
+
+        temp_list = inp.copy()
+        temp_list[y] = temp_list[y][:x] \
+                    + "#" \
+                    + temp_list[y][x + 1:]
+
+        if not find_locations(temp_list)[0]:
+            total += 1
+    
+    return total
 
 def main() -> None:
-    test_input = """"""
+    test_input = """....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."""
     test_input_parsed = test_input.splitlines()
-    test_output_part_1_expected = 0
-    test_output_part_2_expected = 0
+    test_output_part_1_expected = 41
+    test_output_part_2_expected = 6
 
     file_location = os.path.dirname(os.path.realpath(__file__))
     file_location = file_location + "/input.txt"
