0c5f390b75d2cf0e27b84f844acbf3539ab421fb
Rayson Su <rsu@connect.kellettschool.com>
2024-12-08 13:43:51 +0800

    Day 8
    math is hard

--- Day 8/main.py
+++ Day 8/main.py
@@ -3,18 +3,70 @@ from collections import *
 from functools import *
 from itertools import *
 from math import *
+from typing import Any
+
+def is_valid_location(x: int, y: int, 
+                      antenna_locations: dict[Any, list[tuple[int, int]]]) -> bool:
+
+    for locations in antenna_locations.values():
+        for p1, p2 in permutations(locations, 2):
+            if (p2[1] - p1[1]) * (x - p1[0]) \
+            == (y - p1[1]) * (p2[0] - p1[0]) : # modified version of the gradient formula
+                return True
+    
+    return False
 
 def main_part_1(inp: list[str]) -> int:
-    return 0
+    antenna_locations: dict[str, list[tuple[int, int]]] = defaultdict(lambda:[])
+    
+    for y, row in enumerate(inp):
+        for x, char in enumerate(row):
+            if char != ".":
+                antenna_locations[char].append((x, y))
+    
+    antinodes: set[tuple[int, int]] = set()
+    for locations in antenna_locations.values():
+        for p1, p2 in permutations(locations, 2):
+            x0 = 2 * p2[0] - p1[0]
+            y0 = 2 * p2[1] - p1[1]
+        
+            if 0 <= x0 < len(inp[0]) \
+            and 0 <= y0 < len(inp):
+                antinodes.add((x0, y0))
+
+    return len(antinodes)
 
 def main_part_2(inp: list[str]) -> int:
-    return 0
+    antenna_locations: dict[str, list[tuple[int, int]]] = defaultdict(lambda:[])
+    
+    for y, row in enumerate(inp):
+        for x, char in enumerate(row):
+            if char != ".":
+                antenna_locations[char].append((x, y))
+
+    antinodes = 0
+    for x, y in product(range(len(inp[0])), range(len(inp))):
+        if is_valid_location(x, y, antenna_locations):
+            antinodes += 1
+
+    return antinodes
 
 def main() -> None:
-    test_input = """"""
+    test_input = """............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"""
     test_input_parsed = test_input.splitlines()
-    test_output_part_1_expected = 0
-    test_output_part_2_expected = 0
+    test_output_part_1_expected = 14
+    test_output_part_2_expected = 34
 
     file_location = os.path.dirname(os.path.realpath(__file__))
     file_location = file_location + "/input.txt"
