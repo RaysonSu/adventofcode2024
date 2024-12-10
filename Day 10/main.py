e1182c1d8262e232da1d211e0f17891a42b7680d
Rayson Su <rsu@connect.kellettschool.com>
2024-12-10 13:45:08 +0800

    Day 9, 10
    why is day 9 so hard?
    day 10's pretty chill though.

--- Day 10/main.py
+++ Day 10/main.py
@@ -1,20 +1,115 @@
+from __future__ import annotations
+
 import os
 from collections import *
 from functools import *
 from itertools import *
 from math import *
 
+class State():
+    def __init__(self, grid: list[list[int]], x: int, y: int) -> None:
+        self.grid = grid
+        self.x = x
+        self.y = y
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+    
+    def __lt__(self, other: State) -> bool:
+        return hash(self) < hash(other)
+    
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, State) and hash(self) == hash(other)
+    
+    def get_children(self) -> list[State]:
+        children: list[State] = []
+        current_value = self.grid[self.y][self.x]
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nx = self.x + dx
+            ny = self.y + dy
+
+            if not (0 <= nx < len(self.grid[0]) \
+                    and 0 <= ny < len(self.grid)):
+                continue
+            
+            if self.grid[ny][nx] != current_value + 1:
+                continue
+            
+            children.append(State(self.grid, nx, ny))
+        
+        return children
+    
+    def finished(self) -> bool:
+        return self.grid[self.y][self.x] == 9
+
+    def get_score(self) -> int:
+        '''A function that finds the mininum distance to a target node.'''
+        states: deque[tuple[int, State]] = deque()
+        states.append((0, self))
+        found: set[State] = set()
+
+        while states:
+            total_cost, state = states.popleft()
+
+            if state in found:
+                continue
+
+            found.add(state)
+
+            for child in state.get_children():
+                states.append((total_cost + 1, child))
+
+        total = 0
+        for state in found:
+            if state.finished():
+                total += 1
+        
+        return total
+
+    def get_ranking(self) -> int:
+        if self.finished():
+            return 1
+        
+        total = 0
+
+        for child in self.get_children():
+            total += child.get_ranking()
+        
+        return total
+
+
 def main_part_1(inp: list[str]) -> int:
-    return 0
+    grid = [[int(char) for char in row] for row in inp]
+    total = 0
+    for y, row in enumerate(grid):
+        for x, val in enumerate(row):
+            if val == 0:
+                total += State(grid, x, y).get_score()
+
+    return total
 
 def main_part_2(inp: list[str]) -> int:
-    return 0
+    grid = [[int(char) for char in row] for row in inp]
+    total = 0
+    for y, row in enumerate(grid):
+        for x, val in enumerate(row):
+            if val == 0:
+                total += State(grid, x, y).get_ranking()
+
+    return total
 
 def main() -> None:
-    test_input = """"""
+    test_input = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
     test_input_parsed = test_input.splitlines()
-    test_output_part_1_expected = 0
-    test_output_part_2_expected = 0
+    test_output_part_1_expected = 36
+    test_output_part_2_expected = 81
 
     file_location = os.path.dirname(os.path.realpath(__file__))
     file_location = file_location + "/input.txt"
--- Day 9/main.py
+++ Day 9/main.py
@@ -5,16 +5,154 @@ from itertools import *
 from math import *
 
 def main_part_1(inp: list[str]) -> int:
-    return 0
+    if len(inp[0]) % 2 == 0:
+        inp[0] = inp[0][:-1]
+
+    files_remaining = deque(map(list, enumerate(map(int, inp[0]))))
+    
+    adding_front = True
+    total = 0
+    index = 0
+
+    while len(files_remaining) > 1:
+        if adding_front:
+            if files_remaining[0][1] == 0:
+                adding_front = False
+            else:
+                total += files_remaining[0][0] // 2 * index
+                index += 1
+                files_remaining[0][1] -= 1
+        else:
+            if files_remaining[1][1] == 0:
+                files_remaining.popleft()
+                files_remaining.popleft()
+                adding_front = True
+            elif files_remaining[-1][1] == 0:
+                files_remaining.pop()
+                files_remaining.pop()
+            else:
+                total += files_remaining[-1][0] // 2 * index
+                index += 1
+                files_remaining[-1][1] -= 1
+                files_remaining[1][1] -= 1
+    
+    if files_remaining:
+        value, remaining = tuple(files_remaining.pop())
+        for _ in range(remaining):
+            total += value // 2 * index
+            index += 1
+    
+    return total
 
 def main_part_2(inp: list[str]) -> int:
-    return 0
+    if len(inp[0]) % 2 == 0:
+        inp[0] = inp[0][:-1]
 
+    files: list[tuple[int, int]] = []
+    holes: list[int] = []
+
+    for index, size in enumerate(map(int, inp[0])):
+        if index % 2 == 0:
+            files.append((index // 2, size))
+        else:
+            holes.append(size)
+            
+
+    for file_index in range(len(files) - 1, -1, -1):
+        file_location_index, file_size = \
+            next(((f_location, f_size) 
+                  for f_location, (f_index, f_size)
+                  in enumerate(files)
+                  if f_index == file_index))
+
+        
+        hole_location_index, hole_size = \
+            next(((h_location, h_size)
+                  for h_location, h_size
+                  in enumerate(holes[:file_location_index])
+                  if h_size >= file_size), (-1, -1))
+
+        if hole_size == -1:
+            continue
+
+        # remove file
+        files.pop(file_location_index)
+        remvoed_hole_size = holes.pop(file_location_index - 1)
+
+        if file_location_index != len(files):
+            holes[file_location_index - 1] += remvoed_hole_size + file_size
+
+        # put file back
+        files.insert(hole_location_index + 1, (file_index, file_size))
+        holes[hole_location_index] -= file_size
+        holes.insert(hole_location_index, 0)
+
+
+    index = 0
+    total = 0
+    for file_data, hole_size in zip_longest(files, holes):
+        if file_data:
+            file_index, file_size = file_data
+            total += file_index * (index * file_size + file_size * (file_size - 1) // 2)
+            index += file_size
+        
+        if hole_size:
+            index += hole_size
+
+    return total
+
+# def main_part_2(inp: list[str]) -> int:
+#     if len(inp[0]) % 2 == 0:
+#         inp[0] = inp[0][:-1]
+
+#     n = list(enumerate(map(int, inp[0])))
+    
+#     numbers = []
+#     for i, v in n:
+#         if i % 2 == 0:
+#             numbers.append((i // 2, v))
+#         else:
+#             numbers.append((-1, v))
+
+#     for i in range(len(n) // 2, -1, -1):
+#         # print(i)
+#         for j, (vb, sb) in enumerate(numbers):
+#             if vb == i:
+#                 index = j
+#                 break
+        
+#         for k in range(index):
+#             v, s = numbers[k]
+#             if v == -1 and s >= sb:
+#                 numbers.pop(index)
+#                 numbers.insert(index, (-1, sb))
+#                 numbers.pop(k)
+#                 numbers.insert(k, (-1, s - sb))
+#                 numbers.insert(k, (vb, sb))
+#                 break
+        
+#         nnubers = [numbers[0]]
+#         for x, y in numbers[1:]:
+#             if x == nnubers[-1][0]:
+#                 x, t = nnubers.pop()
+#                 nnubers.append((x, y + t))
+#             else:
+#                 nnubers.append((x, y))
+        
+#         numbers = nnubers
+
+#     nt = []
+#     for p, q in numbers:
+#         for _ in range(q):
+#             nt.append(p)
+
+
+#     return sum([i * v for i, v in enumerate(nt) if v != -1])
 def main() -> None:
-    test_input = """"""
+    test_input = """2333133121414131402"""
     test_input_parsed = test_input.splitlines()
-    test_output_part_1_expected = 0
-    test_output_part_2_expected = 0
+    test_output_part_1_expected = 1928
+    test_output_part_2_expected = 2858
 
     file_location = os.path.dirname(os.path.realpath(__file__))
     file_location = file_location + "/input.txt"
