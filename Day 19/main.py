2c8aa14ef797d57e577f72c8f8b726dff9e9e726
Rayson Su <rsu@connect.kellettschool.com>
2024-12-19 17:04:44 +0800

    Day 19
    dynamic programming by beloved

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,20 +1,74 @@
 import os
 from collections import *
+from typing import *
 from functools import *
 from itertools import *
 from math import *
 
+def checker(towels: list[str]) -> Callable[[str], bool]:
+    @cache
+    def valid(design: str) -> bool:
+        if not design:
+            return True
+        
+        for towel in towels:
+            if design.startswith(towel) \
+            and valid(design[len(towel):]):
+                return True
+        
+        return False
+    
+    return valid
+
+def counter(towels: list[str]) -> Callable[[str], int]:
+    @cache
+    def count(towel: str) -> int:
+        if not towel:
+            return 1
+        
+        possibilities = 0
+        for t in towels:
+            if towel.startswith(t):
+                possibilities += count(towel[len(t):])
+        
+        return possibilities
+    
+    return count
+
 def main_part_1(inp: list[str]) -> int:
-    return 0
+    check = checker(inp[0].split(", "))
+
+    total = 0
+    for design in inp[2:]:
+        if check(design):
+            total += 1
+    
+    return total
 
 def main_part_2(inp: list[str]) -> int:
-    return 0
+    count = counter(inp[0].split(", "))
+
+    total = 0
+    for design in inp[2:]:
+        total += count(design)
+    
+    return total
+
 
 def main() -> None:
-    test_input = """"""
+    test_input = """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
     test_input_parsed = test_input.splitlines()
-    test_output_part_1_expected = 0
-    test_output_part_2_expected = 0
+    test_output_part_1_expected = 6
+    test_output_part_2_expected = 16
 
     file_location = os.path.dirname(os.path.realpath(__file__))
     file_location = file_location + "/input.txt"
